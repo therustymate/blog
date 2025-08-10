b952c076641fa132416d63f661ea1a85159deb21
Hyunsung Ra <216290241+therustymate@users.noreply.github.com>
2025-08-10 21:25:35 +1200

    Penetration Tester

--- src/config.ts
+++ src/config.ts
@@ -55,7 +55,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/pfp.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "The Rusty",
-	bio: "Exploit Developer",
+	bio: "Penetration Tester",
 	links: [
 		{
 			name: "GitHub",
