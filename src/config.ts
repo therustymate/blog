e0cfe9607f74bd9ebf33ff0396d969813b0d333a
Hyunsung Ra <216290241+therustymate@users.noreply.github.com>
2025-08-14 09:43:43 +1200

    pfp

--- src/config.ts
+++ src/config.ts
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/pfp.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/pfp.jpeg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "The Rusty",
 	bio: "Penetration Tester",
 	links: [
